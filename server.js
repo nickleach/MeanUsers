d33b40a1742158253bdb6fd94f546aa1e648a8d4
Nick Leach <nickleach22@gmail.com>
2015-10-11 14:10:09 -0400

    Added CRUD for users

--- server.js
+++ server.js
@@ -50,7 +50,95 @@ apiRouter.get('/', function(req, res){
 
 //more routes here
 
-//register routes
+apiRouter.route('/users')
+
+  .post(function(req, res){
+    //create a new instance of the user model
+    var user = new User();
+    // set the users information (comes from requrests)
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+
+    //save the user and check for errors
+    user.save(function(err){
+      if (err){
+        //duplicate entry
+        if(err.code == 11000)
+          return res.json({ success: false, message: "A user with that username already exists. "});
+        else
+          return res.send(err);
+
+      }
+
+          res.json({message: 'User Created!'});
+    })
+
+
+  })
+
+  .get(function(req, res){
+
+    User.find(function(err, users){
+      if (err) res.send(err);
+
+      res.json(users);
+    })
+
+  });
+
+  apiRouter.route('/users/:user_id')
+
+    //get the user with that id
+    .get(function(req, res){
+
+      User.findById(req.params.user_id, function(err, user){
+
+        if(err) res.send(err);
+
+        //return that user
+        res.json(user);
+
+      });
+
+    })
+
+    .put(function(req, res){
+
+      User.findById(req.params.user_id, function(err, user){
+
+        // user our usermodel to find the user we want
+        if(err) res.send(err);
+
+        // update the users info only if its new
+        if(req.body.name) user.name = req.body.name;
+        if(req.body.username) user.username = req.body.username;
+        if(req.body.password) user.password = req.body.password;
+
+        user.save(function(err){
+          if(err) res.send(err);
+
+          res.json({message: 'User updated'});
+
+        });
+
+      });
+
+    })
+
+    .delete(function(req, res){
+      User.remove({
+        _id: req.params.user_id
+      }, function(err, user){
+        if(err) return res.send(err);
+
+        res.json({ message: 'Successfully deleted'});
+      });
+
+
+    });
+
+
 
 app.use('/api', apiRouter);
 
