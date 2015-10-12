988420828ea108c482042e58c51aed64e21937cc
Nick Leach <nickleach22@gmail.com>
2015-10-11 22:53:19 -0400

    added security

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ var express    = require('express'),
     bodyParser = require('body-parser'),
     morgan     = require('morgan'),
     mongoose   = require('mongoose'),
+    jwt        = require('jsonwebtoken'),
+    superSecret     = 'ilovescotchscotchysschotchscotch',
     User       = require('./app/models/user'),
     port       = process.env.PORT || 8080; // set the port
 
 mongoose.connect('mongodb://nick:nick@ds045632.mongolab.com:45632/meanusers');
+
+
 
 // APP CONFIGURATION ---------------------
 // use body parser so we can grab information from POST requests
@@ -25,6 +29,9 @@ app.use(function(req, res, next){
 
 app.use(morgan('dev'));
 
+
+
+
 //Routes
 
 app.get('/', function(req, res){
@@ -34,14 +41,91 @@ app.get('/', function(req, res){
 // get an instance of the express router
 var apiRouter = express.Router();
 
+
+// logging in users
+apiRouter.post('/authenticate', function(req,res){
+
+  //find the user
+  //select the name username and password explicitly
+  User.findOne({
+    username: req.body.username
+  }).select('name username password').exec(function(err, user){
+    if (err) throw err;
+
+    //no user with that username was found
+    if(!user){
+      res.json({
+        success: false,
+        message: 'Authentication failed. User not found.'
+      });
+    } else if (user){
+      // check if password matches
+      var validPassword = user.comparePassword(req.body.password);
+      if (!validPassword){
+        res.json({
+          success: false,
+          message: 'Authentication failed. Wrong password.'
+        });
+      } else {
+        // if user is found and password is right
+        //create token
+        var token = jwt.sign({
+          name: user.name,
+          username: user.username
+        }, superSecret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        //return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        })
+      }
+    }
+  });
+
+});
+
+
+
 //middleware to use for all requrests
 
 apiRouter.use(function (req, res, next){
   //logging
   console.log("someone just came to our app!");
 
-  //this is where we authenticate users
-  next();
+  // check header or url params or post params for token
+
+  var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+  //decode token
+  if (token){
+    //verifies secret and checks exp
+    jwt.verify(token, superSecret, function(err, decoded){
+      if (err) {
+        return res.status(403).send({
+          success: false,
+          message: 'Failed to authenticate token'
+        });
+      }else{
+        //if everything is good save to request for use in other routes
+        req.decoded = decoded;
+
+        next();
+      }
+    });
+} else {
+  //if there is no token
+  // return an HTTP response of 403 (access forbidden) and an error message
+
+  return res.status(403).send({
+    success: false,
+    message: 'No token provided.'
+  });
+
+  }
 });
 
 apiRouter.get('/', function(req, res){
@@ -138,7 +222,9 @@ apiRouter.route('/users')
 
     });
 
-
+apiRouter.get('/me', function(req, res){
+  res.send(req.decoded);
+});
 
 app.use('/api', apiRouter);
 
